3d5b16df586db03cb5085629b570766b33eaf145
Chae Lin Shin <chaelinshin96@gmail.com>
2021-07-07 17:09:17 +0900

    Add React - JSX rules

--- React/react-tutorial/begin-react/src/App.js
+++ React/react-tutorial/begin-react/src/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import Hello from './Hello';
 
 function App() {
+  //JSX 규칙 
+  // 4. 괄호는 필수가 아니다. 그러나 가독성을 위해 사용한다. 
   return (
+    // 2. 2개 이상의 태그가 있다면, 하나의 태그로 감싸져야 한다. 
     <div>
+      {/* 1. 태그는 꼭 닫아야 한다. */}
       <Hello />
-      <Hello />
-      <Hello />
+      
+      <input/>
+
+      {/* 3. 감싸기 애매할 때는 Fragment를 사용해도 된다. Fragment: <> */}
+      <>
+      </>
     </div>
   );
 }
--- React/react-tutorial/begin-react/src/Hello.js
+++ React/react-tutorial/begin-react/src/Hello.js
@@ -3,6 +3,7 @@ import React from 'react';
 // 함수형 컴포넌트
 function Hello() {
     // jsx (semicolon은 생략 가능)
+    // jsx로 작성한 코드는 Javascritp로 변환이 된다 (babeljs.io에서 확인 가능)
     return <div>안녕하세요</div>;
 }
 
