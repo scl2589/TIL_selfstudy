01da885a55993e9559e069fe640ff0b2c92968e3
Chae Lin Shin <chaelinshin96@gmail.com>
2021-07-10 12:51:49 +0900

    Update React - 조건부 렌더링

--- React/react-tutorial/begin-react/src/App.js
+++ React/react-tutorial/begin-react/src/App.js
@@ -8,6 +8,8 @@ function App() {
     <Wrapper>
       {/* isSpecial = {true} 로 설정할 때, true 또한 JS 값이기 때문에 중괄호로 감싸줘야 한다. */}
       <Hello name="react" color="red" isSpecial={true}/>
+      {/* isSpecial처럼 값을 지정하지 않는다면, true로 지정한 것과 동일하다. */}
+      <Hello name="react" color="red" isSpecial/>
       <Hello color="pink"/>
     </Wrapper>
   );
