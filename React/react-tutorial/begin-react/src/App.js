3efd7b94024d19067fc2fd04a76dc84f9e872f1d
Chae Lin Shin <chaelinshin96@gmail.com>
2021-07-22 13:32:20 +0900

    React - Adding to array

--- React/react-tutorial/begin-react/src/App.js
+++ React/react-tutorial/begin-react/src/App.js
@@ -1,9 +1,24 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import UserList from './UserList'
+import CreateUser from './CreateUser';
 import './App.css';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  })
+
+  const {username, email} = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs, 
+      [name]: value, 
+    })
+  }
+
+  const [users, setUsers]  = useState([
     {
         id: 1,
         username: 'Lin',
@@ -19,10 +34,25 @@ function App() {
         username: 'Manfred',
         email: 'manfred@example.com'
     }
-  ]  
+  ]);  
 
   const nextId = useRef(4); // 초기값을 4로 한다. 
+
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+
+    // setUsers([...users, user]) // spread 연산자를 사용하고, 새로운 user를 넣어주는 방법
+    setUsers(users.concat(user)); // concat을 사용해 기존 users 배열에 새로운 user를 합친다. 
+
+    setInputs({
+      username: '',
+      email: ''
+    });
+
     console.log(nextId.current); // 4 
     
     // 값을 바꾸고 싶을 때는?? - oncreate 함수를 실행할 때 마다 nextId를 사용한다. 
@@ -30,7 +60,15 @@ function App() {
   }
 
   return (
-    <UserList users={users}/>
+    <>
+      <CreateUser 
+        username={username} 
+        email={email} 
+        onChange={onChange} 
+        onCreate={onCreate}
+      />
+      <UserList users={users}/>
+    </>
   );
 }
 
