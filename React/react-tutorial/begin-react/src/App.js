8ff4c8da23993201807669ce18ac5e80c4bf8ba3
Chae Lin Shin <chaelinshin96@gmail.com>
2021-07-23 09:12:27 +0900

    Add React - useMemo

--- React/react-tutorial/begin-react/src/App.js
+++ React/react-tutorial/begin-react/src/App.js
@@ -1,7 +1,12 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useMemo } from 'react';
 import UserList from './UserList'
 import CreateUser from './CreateUser';
 import './App.css';
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는 중...');
+  return users.filter(user => user.active).length;
+}
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -78,6 +83,8 @@ function App() {
     ));
   }
 
+  const count = useMemo(() =>countActiveUsers(users));
+
   return (
     <>
       <CreateUser 
@@ -87,6 +94,7 @@ function App() {
         onCreate={onCreate}
       />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      <div>활성 사용자 수: {count} </div>
     </>
   );
 }
