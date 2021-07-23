0f7e75642a62bb53832f2b5fef186cca65a82931
Chae Lin Shin <chaelinshin96@gmail.com>
2021-07-23 10:50:21 +0900

    Add React - useCallback

--- React/react-tutorial/begin-react/src/App.js
+++ React/react-tutorial/begin-react/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useMemo } from 'react';
+import React, { useState, useRef, useMemo, useCallback } from 'react';
 import UserList from './UserList'
 import CreateUser from './CreateUser';
 import './App.css';
@@ -15,14 +15,15 @@ function App() {
   })
 
   const {username, email} = inputs;
-
-  const onChange = e => {
+  // onChange 함수는 inputs가 바뀔 때만 함수가 새로 만들어지고,
+  // 그렇지 않다면 기존에 만들어진 함수를
+  const onChange = useCallback(e => {
     const { name, value } = e.target;
     setInputs({
       ...inputs, 
       [name]: value, 
     })
-  }
+  }, [inputs]);
 
   const [users, setUsers]  = useState([
     {
@@ -47,7 +48,7 @@ function App() {
 
   const nextId = useRef(4); // 초기값을 4로 한다. 
 
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
@@ -66,22 +67,22 @@ function App() {
     
     // 값을 바꾸고 싶을 때는?? - oncreate 함수를 실행할 때 마다 nextId를 사용한다. 
     nextId.current += 1; 
-  }
+  }, [username, email, users]);
 
-  const onRemove = id => {
+  const onRemove = useCallback(id => {
     // filter를 걸어서 각 user 객체를 확인해본다.
     // parameter로 받아온 id를 비교하고, 만약 만족하는 경우 새로운 배열을 만들어서, 배열에 넣고. 
     // 만족하지 않는 경우, 새로운 배열에 넣지 않는다. 
     setUsers(users.filter(user => user.id !== id));
-  }
+  }, [users]);
 
-  const onToggle = id => {
+  const onToggle = useCallback(id => {
     setUsers(users.map(
       user => user.id === id
       ? { ...user, active: !user.active }
       : user 
     ));
-  }
+  }, [users]);
 
   const count = useMemo(() =>countActiveUsers(users));
 
