d0735425944ffb07f1d6db58cefe494f4a71105f
Chae Lin Shin <chaelinshin96@gmail.com>
2021-07-28 11:10:25 +0900

    Add React - intro to class component

--- React/react-tutorial/begin-react/src/index.js
+++ React/react-tutorial/begin-react/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './Hello';
 
 // ReactDOM은 페이지에서 getElementById - ID가 root인 것을 찾아서 넣겠다 라는 뜻이다.
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Hello name="Lin" isSpecial="true"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
