3f3dc36cb6a7d42af6d885921cb54b9ac90a398f
Chae Lin Shin <chaelinshin96@gmail.com>
2021-08-02 11:14:43 +0900

    Add React - using Snippet

---
+++ React/react-tutorial/useful-tools/Sample.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ${TM_FILENAME_BASE}(){
+  return;
+  <div>Sample</div>;
+}
+
+export default ${TM_FILENAME_BASE};
+
+//https://snippet-generator.app/ snippet generator -> javascriptreact.json에 저장 
