1c9bd6e3748475c22c8262fa6a9214dc78474192
Chae Lin Shin <chaelinshin96@gmail.com>
2021-08-02 15:39:19 +0900

    Add React - sass size props

--- React/react-tutorial/styling-with-sass/src/components/Button.js
+++ React/react-tutorial/styling-with-sass/src/components/Button.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './Button.scss';
+import classNames from 'classnames';
 
-function Button({children}){
-  return <button className="Button">{children}</button>
+function Button({children, size = 'medium'}){
+//   return <button className={`Button ${size}`}>{children}</button>
+    // return <button className={classNames('Button', size || 'medium')}>{children}</button>
+    return <button className={classNames('Button', size)}>{children}</button>
+}
+
+Button.defaultProps = {
+    size: 'medium'
 }
 
 export default Button;
