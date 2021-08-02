88c7d2d8c684bc6ba369b61069092dcb606712b3
Chae Lin Shin <chaelinshin96@gmail.com>
2021-08-02 16:05:12 +0900

    Add React - sass color props

--- React/react-tutorial/styling-with-sass/src/components/Button.js
+++ React/react-tutorial/styling-with-sass/src/components/Button.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './Button.scss';
 import classNames from 'classnames';
 
-function Button({children, size = 'medium'}){
+function Button({children, size = 'medium', color}){
 //   return <button className={`Button ${size}`}>{children}</button>
     // return <button className={classNames('Button', size || 'medium')}>{children}</button>
-    return <button className={classNames('Button', size)}>{children}</button>
+    return <button className={classNames('Button', size, color)}>{children}</button>
 }
 
 Button.defaultProps = {
-    size: 'medium'
+    size: 'medium',
+    color: 'blue'
 }
 
 export default Button;
