21be41eb625dd77bbcf8815766281a234f559fa4
Chae Lin Shin <chaelinshin96@gmail.com>
2021-08-03 16:02:07 +0900

    Add React - make dialog with styled components

--- React/react-tutorial/styling-with-styled-components/src/App.js
+++ React/react-tutorial/styling-with-styled-components/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, {ThemeProvider} from 'styled-components';
-import Button from './components/Button'
+import Button from './components/Button';
+import Dialog from './components/Dialog';
 
 const AppBlock = styled.div`
   width: 512px;
@@ -23,12 +24,31 @@ const ButtonGroup = styled.div`
 `
 
 function App() {
+  const [dialog, setDialog] = useState(false);
+  const onClick = () => {
+    setDialog(true);
+  }
+  const onConfirm = () => {
+    console.log('확인')
+    setDialog(false);
+  }
+
+  const onCancel = () => {
+    console.log('취소')
+    setDialog(false);
+  }
+
   return (
     <ThemeProvider theme={{
       palette
     }}>
+      <>
+
       <AppBlock>
-        <ButtonGroup>
+        <Button color="pink" size="large" onClick={onClick}>
+          삭제
+        </Button>
+        {/* <ButtonGroup>
           <Button color="pink" size="large">BUTTON</Button>
           <Button>BUTTON</Button>
           <Button size="small" color="gray">BUTTON</Button>
@@ -42,8 +62,19 @@ function App() {
           <Button color="pink" size="large" fullWidth>BUTTON</Button>
           <Button size="large" fullWidth>BUTTON</Button>
           <Button size="large" color="gray" fullWidth>BUTTON</Button>
-        </ButtonGroup>
+        </ButtonGroup> */}
       </AppBlock>
+      <Dialog 
+          title="정말로 삭제하시겠습니까?"
+          confirmText="삭제"
+          cancelText="취소"
+          onConfirm={onConfirm}
+          onCancel={onCancel}
+          visible={dialog}
+        >
+          데이터를 정말로 삭제하시겠습니까?
+        </Dialog>
+      </>
     </ThemeProvider>
 
   );
