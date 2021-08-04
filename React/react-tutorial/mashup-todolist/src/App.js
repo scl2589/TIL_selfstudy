2ab759705ccff5ac86e9710457a7fda40a02451e
Chae Lin Shin <chaelinshin96@gmail.com>
2021-08-04 13:34:08 +0900

    Add React - TodoList UI 1

---
+++ React/react-tutorial/mashup-todolist/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components'
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoTemplate from './components/TodoTemplate';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <TodoTemplate className="App">
+        <TodoHead />
+        <TodoList />
+      </TodoTemplate>
+    </>
+
+  );
+}
+
+export default App;
