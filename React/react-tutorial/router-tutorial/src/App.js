69edec81231e36e39643726df1b1ab0f0bc6bdcd
Chae Lin Shin <chaelinshin96@gmail.com>
2021-08-05 17:12:59 +0900

    Add React - router parameter and query

--- React/react-tutorial/router-tutorial/src/App.js
+++ React/react-tutorial/router-tutorial/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Profile from './Profile'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       </ul>
       <Route path="/" component={Home}  exact/>
       <Route path='/about' component={About}/>
+      <Route path='/profiles/:username' component={Profile} />
     </div>
   );
 }
