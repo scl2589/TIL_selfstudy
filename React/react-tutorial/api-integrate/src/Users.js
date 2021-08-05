d8be4838c3cc87fde24becff14938c13ab0ad2aa
Chae Lin Shin <chaelinshin96@gmail.com>
2021-08-05 15:06:57 +0900

    Add React - UsersContext

--- React/react-tutorial/api-integrate/src/Users.js
+++ React/react-tutorial/api-integrate/src/Users.js
@@ -1,24 +1,33 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 // import useAsync from './useAsync';
-import { useAsync } from 'react-async';
+// import { useAsync } from 'react-async';
+import { useUsersState, useUsersDispatch, getUsers } from './UsersContext';
 import User from './User';
 
-async function getUsers() {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users/');
-    return response.data;
-}
+// async function getUsers() {
+//     const response = await axios.get('https://jsonplaceholder.typicode.com/users/');
+//     return response.data;
+// }
 
 
 function Users() {
     const [userId, setUserId] = useState(null);
-    const { data:users, error, isLoading, reload, run} = useAsync({
-        deferFn:getUsers
-    })
+    // const { data:users, error, isLoading, reload, run} = useAsync({
+    //     deferFn:getUsers
+    // })
+    const state = useUsersState();
+    const dispatch = useUsersDispatch();
 
-    if (isLoading) return <div>로딩중...</div>
+    const { loading, data : users, error } = state.users;
+
+    const fetchData = () => {
+        getUsers(dispatch); 
+    }
+
+    if (loading) return <div>로딩중...</div>
     if (error) return <div>에러가 발생했습니다</div>
-    if (!users) return <button onClick={run}>불러오기</button>;
+    if (!users) return <button onClick={fetchData}>불러오기</button>;
 
     return (
     <>
@@ -28,7 +37,7 @@ function Users() {
                 {user.username} ({user.name})
             </li>)}
         </ul>
-        <button onClick={reload}>다시 불러오기</button>
+        <button onClick={fetchData}>다시 불러오기</button>
         { userId && <User id={userId} />}
     </>
     )
