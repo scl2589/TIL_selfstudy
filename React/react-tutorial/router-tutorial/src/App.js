ed51d215c3faec58d84ff438ede975c14a2d6d81
Chae Lin Shin <chaelinshin96@gmail.com>
2021-08-05 17:20:43 +0900

    Add React - router subroute

--- React/react-tutorial/router-tutorial/src/App.js
+++ React/react-tutorial/router-tutorial/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
-import Profile from './Profile'
+import Profiles from './Profiles'
 
 function App() {
   return (
@@ -14,10 +14,13 @@ function App() {
         <li>
           <Link to="/about"> About</Link>
         </li>
+        <li>
+          <Link to="/profiles">프로필 목록</Link>
+        </li>
       </ul>
       <Route path="/" component={Home}  exact/>
       <Route path='/about' component={About}/>
-      <Route path='/profiles/:username' component={Profile} />
+      <Route path='/profiles' component={Profiles} />
     </div>
   );
 }
