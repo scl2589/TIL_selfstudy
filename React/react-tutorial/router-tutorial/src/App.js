f322879c66fcf861fe1cd42d7dad3bfc2d930c27
scl2589 <chaelinshin96@gmail.com>
2021-08-06 10:18:07 +0900

    Add React - router switch

--- React/react-tutorial/router-tutorial/src/App.js
+++ React/react-tutorial/router-tutorial/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Profiles from './Profiles'
@@ -22,10 +22,17 @@ function App() {
           <Link to="/history">예제</Link>
         </li>
       </ul>
-      <Route path="/" component={Home}  exact/>
-      <Route path='/about' component={About}/>
-      <Route path='/profiles' component={Profiles} />
-      <Route path="/history" component={HistorySample} />
+      {/* Not found 페이지를 보여줄 때 Switch를 사용하면 유용하다. */}
+      <Switch>
+        <Route path="/" component={Home}  exact/>
+        <Route path='/about' component={About}/>
+        <Route path='/profiles' component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route render={({ location }) => <div>
+          <h2>이 페이지는 존재하지 않습니다.</h2>
+          <p>{ location.pathname }</p>
+          </div>} />
+      </Switch>
     </div>
   );
 }
