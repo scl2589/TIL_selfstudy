baaefe0716362fcbd5aa19f3050c768c2861254d
scl2589 <chaelinshin96@gmail.com>
2021-08-06 10:31:27 +0900

    Add React - router useReactRouterHook

--- React/react-tutorial/router-tutorial/src/Profiles.js
+++ React/react-tutorial/router-tutorial/src/Profiles.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Profile from './Profile';
-import { NavLink, Route } from 'react-router-dom';
+import { Link, NavLink, Route } from 'react-router-dom';
 import WithRouterSample from './WithRouterSample';
+import RouterHookSample from './RouterHookSample';
 
 function Profiles(){
   return (
@@ -23,6 +24,7 @@ function Profiles(){
           <Route path="/profiles" exact render={() => <div>사용자를 선택해주세요</div>} />
           <Route path="/profiles/:username" component={Profile}/>
           <WithRouterSample/>
+          <RouterHookSample />
       </div>
   )
 }
--- React/react-tutorial/router-tutorial/src/WithRouterSample.js
+++ React/react-tutorial/router-tutorial/src/WithRouterSample.js
@@ -5,9 +5,9 @@ function WithRouterSample({ location, match, history }){
   return (
       <div>
           <h4>location</h4>
-          <textarea value={JSON.stringify(location, null, 2)} readonly/>
+          <textarea value={JSON.stringify(location, null, 2)} readOnly/>
           <h4>match</h4>
-          <textarea value={JSON.stringify(match, null, 2)} readonly/>
+          <textarea value={JSON.stringify(match, null, 2)} readOnly/>
           <button onClick={() => history.push('/')}>홈으로</button>
       </div>
   )
